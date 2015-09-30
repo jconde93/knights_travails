4031a85846c728598afdc0f09036dde16e1057d5
Janner Conde <condejanner@gmail.com>
2015-09-29 20:10:07 -0600

    basic node

---
+++ node.rb
@@ -0,0 +1,35 @@
+class Node
+  attr_reader :position, :moves, :route
+
+  def initialize arr, route=[]
+    if valid? arr, route
+      @position = arr
+      @route = route + [arr]
+    end
+
+    @moves = []
+
+    x = arr[0]
+    y = arr[1]
+
+    possible_moves = [[x+2,y+1],[x+1,y+2],[x-1,y+2],[x-2,y+1]\
+                      ,[x-2,y-1],[x-1,y-2],[x+1,y-2],[x+2,y-1]]
+
+    possible_moves.each do |move|
+      if valid? move, route
+        @moves.push move
+      end
+    end
+  end
+
+  private
+  def valid? arr, route=[]
+    if arr[0] < 0 or arr[0] > 7 \
+      or arr[1] < 0 or arr[1] > 7 \
+      or route.include? arr
+      return false
+    else
+      return true
+    end
+  end
+end
